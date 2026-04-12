660aeb0a769fd9032905e5d643f1422504248746
Michal Grzesiak <darkjjookkeerr@gmail.com>
2026-04-12 22:55:42 +0200

    feat: prettier initial code

--- app/modal.tsx
+++ app/modal.tsx
@@ -1,10 +1,9 @@
-import { Link } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
+import { Link } from "expo-router";
+import { StyleSheet } from "react-native";
 
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-
-export default function ModalScreen() {
+const ModalScreen = () => {
   return (
     <ThemedView style={styles.container}>
       <ThemedText type="title">This is a modal</ThemedText>
@@ -13,13 +12,13 @@ export default function ModalScreen() {
       </Link>
     </ThemedView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     padding: 20,
   },
   link: {
@@ -27,3 +26,5 @@ const styles = StyleSheet.create({
     paddingVertical: 15,
   },
 });
+
+export default ModalScreen;
