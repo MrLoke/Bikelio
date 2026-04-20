e80e7f8d66e5e2018cd8c6310d18a77eb8eb0e89
Michal Grzesiak <darkjjookkeerr@gmail.com>
2026-04-20 16:51:05 +0200

    feat: add i18n config

---
+++ i18n/index.ts
@@ -0,0 +1,21 @@
+import * as Localization from "expo-localization";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import en from "./en.json";
+import pl from "./pl.json";
+
+const resources = {
+  en: { translation: en },
+  pl: { translation: pl },
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: Localization.getLocales()[0].languageCode ?? "en",
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
