d764ee469b9c10802fdbb7712b64277e88f95142
Michal Grzesiak <darkjjookkeerr@gmail.com>
2026-04-20 16:49:36 +0200

    build: add tamagui metro plugin

--- metro.config.js
+++ metro.config.js
@@ -1,14 +1,22 @@
 const { getDefaultConfig } = require("expo/metro-config");
+const { withTamagui } = require("@tamagui/metro-plugin");
 
+/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
 config.transformer = {
+  ...config.transformer,
   babelTransformerPath: require.resolve("react-native-svg-transformer"),
 };
 
 config.resolver = {
+  ...config.resolver,
   assetExts: config.resolver.assetExts.filter((ext) => ext !== "svg"),
   sourceExts: [...config.resolver.sourceExts, "svg"],
 };
 
-module.exports = config;
+module.exports = withTamagui(config, {
+  components: ["tamagui"],
+  config: "./tamagui.config.ts",
+  outputCSS: "./tamagui.css",
+});
